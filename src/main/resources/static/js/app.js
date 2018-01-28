6f136ebe3409e62b4c163a21c733e4dd88d51808
maxtotheguenther <maxtotheguenther@gmail.com>
2018-01-28 19:21:05 +0100

    Improved Style for menu

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,9 +5,7 @@ $(document).ready(function () {
         var currentWindowsSize = $(window).width();
         if (currentWindowsSize < 767) {
             $(".dropdown-content").slideUp();
-            $(".dropdown").removeClass("border-shadow-upper-lower");
             $(this).find(".dropdown-content").slideDown();
-            $(this).addClass("border-shadow-upper-lower");
         }
     });
 
